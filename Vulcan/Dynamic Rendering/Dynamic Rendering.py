a7f63969d2d2ec3b7f7cc037cb8508076eaa48da
Strutter Beast <bbz.rar@gmail.com>
2026-07-04 02:04:17 -0700

    1.9.01
    update Dynamic rendering

--- Vulcan/Dynamic Rendering/Dynamic Rendering.py
+++ Vulcan/Dynamic Rendering/Dynamic Rendering.py
@@ -2,5 +2,7 @@ import vulkan as vk
 
 # 1 Enable tis Valkin 1.3 Dynamic rendering feature struct
 dynamic_render_features = vk . VkPhysicalDeviceDynamicRenderFeatures(
-    
+   sType=vk.VKSTRUCTURE_TYPE_PHYSICAL_DYNAMIC_REDERING_FEATURES,
+   pNext=None,
+   dynamicRender=vk.VK_TURE # skips legacy RenderPass requirements
 )
